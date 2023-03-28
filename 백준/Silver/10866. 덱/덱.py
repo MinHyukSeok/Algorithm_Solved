fdb78f8d670e5946dd2c653de87db5acf7f72ee1
MinHyuk Seok <122436511+MinHyukSeok@users.noreply.github.com>
2023-03-28 17:30:21 +0900

    [Silver IV] Title: 덱, Time: 192 ms, Memory: 117616 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+print = sys.stdout.write
+
+
+N = int(input())
+queue = deque()
+for _ in range(N):
+    t = list(input().split())
+    if t[0] == 'push_back':
+        queue.append(int(t[1]))
+    elif t[0] == 'push_front':
+        queue.appendleft(int(t[1]))
+    elif t[0] == 'pop_back':
+        if len(queue):
+            print(f'{queue.pop()}\n')
+        else:
+            print('-1\n')
+    elif t[0] == 'pop_front':
+        if len(queue):
+            print(f'{queue.popleft()}\n')
+        else:
+            print('-1\n')
+    elif t[0] == 'size':
+        print(f'{len(queue)}\n')
+    elif t[0] == 'empty':
+        if len(queue) == 0:
+            print('1\n')
+        else:
+            print('0\n')
+    elif t[0] == 'back':
+        if len(queue) == 0:
+            print('-1\n')
+        else:
+            print(f'{queue[-1]}\n')
+    elif t[0] == 'front':
+        if len(queue) == 0:
+            print('-1\n')
+        else:
+            print(f'{queue[0]}\n')
