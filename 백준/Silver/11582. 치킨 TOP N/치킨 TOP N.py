54501442dc9b8fca3c4851d26a64d08cd094dcdf
MinHyuk Seok <122436511+MinHyukSeok@users.noreply.github.com>
2023-03-31 15:49:52 +0900

    [Silver IV] Title: 치킨 TOP N, Time: 664 ms, Memory: 362556 KB -BaekjoonHub

--- 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
+++ 백준/Silver/11582. 치킨 TOP N/치킨 TOP N.py
@@ -4,10 +4,10 @@ sys.setrecursionlimit(10**5)
 
 
 def merge(arr, c):
-    if c == 0 or len(arr) == 1:
+    if c == 0 or len(arr) == 1:             # 합칠때 정렬
         return sorted(arr)
 
-    middle = len(arr) // 2
+    middle = len(arr) // 2                  # 분해
     left = merge(arr[:middle], c - 1)
     right = merge(arr[middle:], c - 1)
     res = left + right
@@ -20,6 +20,3 @@ k = int(input())
 
 r = merge(lst, int(math.log2(k)))
 print(*r)
-
-# print(int(math.log2(1)))
-# print(merge(lst, int(math.log2(1))))
